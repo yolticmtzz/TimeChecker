da9b346d39c12c48696011ddb06ff97f534ff799
panovjos <panovjos@students.zhaw.ch>
2021-12-17 22:44:33 +0100

    Add nuget package EF Design

---
+++ TimeCheckerWPF5.0/Class1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeChecker.DAL.Data;
+using TimeChecker.DAL.Models;
+
+namespace TimeCheckerWPF5._0
+{
+    public class Class1
+    {
+
+        //private readonly ApplicationDbContext _context;
+
+        //public Class1(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public void Index()
+        //{
+        //    var timeentryd = _context.Timeentry.ToList();
+
+        //}
+
+        //public void Index3()
+        //{
+        //    var d = _context.Timeentry;
+         
+
+        //}
+
+
+
+    }
+
+}
