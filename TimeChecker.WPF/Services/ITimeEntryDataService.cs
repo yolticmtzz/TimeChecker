8f138d25d515ed47ba03078f794692b1b164b603
CptDestiny <stef.guler@gmail.com>
2021-12-24 11:17:12 +0100

    Backup

---
+++ TimeChecker.WPF/Services/ITimeEntryDataService.cs
@@ -0,0 +1,11 @@
+﻿using TimeChecker.Domain.Models;
+using TimeChecker.EntityFramework;
+
+namespace TimeChecker.WPF.Services
+{
+    public interface ITimeEntryDataService
+    {
+        void CreateTimeEntry(int type);
+
+    }
+}
---
+++ TimeChecker.WPF/Services/TimeEntryDataService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using TimeChecker.Domain.Models;
+using TimeChecker.EntityFramework;
+
+namespace TimeChecker.WPF.Services
+{
+    class TimeEntryDataService : ITimeEntryDataService
+    {
+        private readonly TimeCheckerDbContextFactory _context;
+        
+        public TimeEntryDataService(TimeCheckerDbContextFactory context, int type)
+        {
+            _context = context;
+            CreateTimeEntry(type);
+        }
+
+        public void CreateTimeEntry(int type)
+        {
+            
+            var record = new TimeEntry()
+            {
+                User = "DummyUser",
+                Type = type,
+                DateTimNow = DateTime.Now,
+                Comment = ""
+            };
+
+            var _contextr = _context.CreateDbContext();
+
+            //How now write this to de Database?
+            _contextr.TimeEntries.Add(record);
+            _contextr.SaveChanges();
+
+
+            var timeentry = new Dictionary<string, string>();
+            timeentry.Add("user", "DummyUser");
+            timeentry.Add("type", type.ToString());
+            timeentry.Add("datetimenow", DateTime.Now.ToString());
+            timeentry.Add("comment", "");
+
+            string dictset = "";
+            foreach (var element in timeentry)
+            {
+                dictset = dictset + $" {element},";
+            }
+            
+            MessageBox.Show(dictset);
+        }
+    }
+}
+
+
+
+
+  
+
