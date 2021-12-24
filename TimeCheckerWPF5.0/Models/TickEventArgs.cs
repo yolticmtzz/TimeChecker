9ce21c118c71d75e44549b519e95753f0ee04ce7
CptDestiny <stef.guler@gmail.com>
2021-12-24 11:44:19 +0100

    Including Timewatch

---
+++ TimeCheckerWPF5.0/Models/TickEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCheckerWPF5._0.Models
+{
+    class TickEventArgs
+    {
+        //Data carrier of the Timespans and it must inherit the EventArgs. To be used with the EventHandler
+        public class TickEventArgs : EventArgs
+
+        {
+            public TimeSpan TimeSpan { get; set; }
+        }
+    }
+
+}
