e4721dc065f3fb7afc5b56b38d72dc9beaa89b60
panovjos <panovjos@students.zhaw.ch>
2022-01-09 15:48:12 +0100

    Timentry data save in txt datei with stream

--- TimeChecker/Controllers/TimeentryController.cs
+++ TimeChecker/Controllers/TimeentryController.cs
@@ -19,38 +19,13 @@ namespace TimeChecker.Controllers
         // Variable für Datenbankinhalt
         private readonly ApplicationDbContext _context;
 
-        const string path = @"C:\Users\jopa\Desktop\Test Filestream\SerializeData.txt";
-
-
-
+        const string pathtxt = @"C:\Users\jopa\Desktop\Test Filestream\SerializeData.txt";
 
 
         // Dependency injection Übergabe des Datenbankinhalts
         public TimeentryController(ApplicationDbContext context)
         {
             _context = context;
-        }
-
-        public IActionResult Export()
-        {
-            //SerializeData SD = new SerializeData("Test");
-            //BinaryFormatter Formatter = new BinaryFormatter();
-            //FileStream stream = new FileStream(@"C:\Users\jopa\Desktop\Test Filestream\SerializeData.txt", FileMode.Create, FileAccess.Write);
-            var textToSave = "Hallo";
-
-            SaveWithStream(path, textToSave);
-            //ReadWithStream(path);
-
-            return View("Index");
-        }
-
-        private static void SaveWithStream(string path, string textToSave)
-        {
-            using (var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
-            {
-                var encodedText = Encoding.ASCII.GetBytes(textToSave);
-                fs.Write(encodedText, 0, encodedText.Length);
-            }
         }
 
         public IActionResult Index()
@@ -120,6 +95,68 @@ namespace TimeChecker.Controllers
 
             return RedirectToAction("Index");
 
+        }
+
+        // Timeentry Daten in Textdatei exportieren
+        public IActionResult Export()
+        {
+            var timeentryinDB = _context.Timeentry.ToList();
+
+            SaveWithStream(pathtxt, timeentryinDB);
+
+            return RedirectToAction("Index");
+        }
+
+        // Textdatei via stream speichern
+        private static void SaveWithStream(string path, List<Timeentry> timeentryinDB)
+        {
+            var convertString = "";
+
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (TextWriter writer = new StreamWriter(fs))
+            {
+            
+                foreach (var timeentry in timeentryinDB)
+                {
+
+                        switch (timeentry.Type)
+                        {
+                            case 1:
+                            convertString = timeentry.Type.ToString("Check In");
+                            break;
+                            
+                            case 2:
+                            convertString = timeentry.Type.ToString("Check Out");
+                            break;
+
+                            case 3:
+                            convertString = timeentry.Type.ToString("Start Break");
+                            break;
+
+                            case 4:
+                            convertString = timeentry.Type.ToString("Stop Break");
+                            break;
+
+                            default: 
+                            break;
+
+                        }
+
+                    if (timeentry.Comment == null)
+                    {
+                        timeentry.Comment = "Leer";
+                    }
+
+                    writer.WriteLine(convertString);
+                    writer.WriteLine(timeentry.DateTime);
+                    writer.WriteLine(timeentry.Comment);
+                    writer.WriteLine(timeentry.User);
+                    writer.WriteLine();
+                }
+
+                 
+                
+            }
         }
     }
 }
