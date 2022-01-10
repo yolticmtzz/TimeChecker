0fa992fd10e70d4c7f6a667846b605e6a2ef4702
CptDestiny <stef.guler@gmail.com>
2022-01-10 09:25:36 +0100

    Clean Code

---
+++ TimeCheckerWPF5.0/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+using TimeChecker.DAL.Models;
+using TimeCheckerWPF5._0.Commands;
+using TimeCheckerWPF5._0.Models;
+using TimeCheckerWPF5._0.Services;
+using TimeCheckerWPF5._0.Stores;
+
+namespace TimeCheckerWPF5._0.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _firstname;
+        public string Firstname
+        {
+            get => _firstname;
+
+            set
+            {
+                _firstname = value;
+                RaisePropertyChanged(nameof(Firstname));
+            }
+        }
+
+        private string _lastName;
+        public string Lastname
+        {
+            get => _lastName;
+
+            set
+            {
+                _firstname = value;
+                RaisePropertyChanged(nameof(Lastname));
+            }
+        }
+
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel(UserStore userStore, INavigationService loginNavigationService)
+        {
+            LoginCommand = new LoginCommand(this, userStore, loginNavigationService);
+        }
+
+
+
+    }
+}
