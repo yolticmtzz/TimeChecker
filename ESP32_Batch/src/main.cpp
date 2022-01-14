c82c56e4bfda83aad4b68acc86d9c6a120375deb
panovjos <panovjos@students.zhaw.ch>
2022-01-14 16:53:56 +0100

    clean up

--- ESP32_Batch/src/main.cpp
+++ ESP32_Batch/src/main.cpp
@@ -1,6 +1,22 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+ /** Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
+ * 
+*/
+
 #define SS_PIN 5
 #define RST_PIN 0
 
@@ -25,7 +41,7 @@ void setup()
 void loop()
 {
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  // Überprüfung ob neue Karte anwesend und diese gelesen werden kann
   if (Batch.PICC_IsNewCardPresent() && Batch.PICC_ReadCardSerial())
   {
 
@@ -33,10 +49,11 @@ void loop()
     {
       for (int j = 0; j < 4; j++)
       {
-        // Batch nicht gleich Checkout ID ist, dann check = true
+        // Überprüfung ob Batch-ID-CheckIN ungleich Batch-ID-CheckOUT ist.
         if (Batch.uid.uidByte[j] != batch_uid_checkOUT[j])
         {
           Serial.print("Gelesene UID: ");
+          // Batch-ID-CheckIN ausgeben
           for (byte i = 0; i < Batch.uid.size; i++)
           {
             Serial.print(Batch.uid.uidByte[i] < 0x10 ? "0" : " ");
@@ -49,9 +66,11 @@ void loop()
       }
     }
 
+    // Überprüfung ob Batch-ID-CheckIN gleich Batch-ID-CheckOUT ist.
     else 
     {
       Serial.print("Gelesene UID: ");
+      // Batch-ID-CheckOUT beschreiben und ausgeben
       for (int x = 0; x < 4; x++)
       {
         Batch.uid.uidByte[x] = batch_uid_checkOUT[x];
@@ -63,7 +82,6 @@ void loop()
       Serial.println();
       check = false;
     }
-
 
     
     // Halt PICC
