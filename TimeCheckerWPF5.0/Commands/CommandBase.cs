ca28e267d3a7a55a453b5c8f75e2a54d5221f0f0
CptDestiny <stef.guler@gmail.com>
2022-01-21 19:50:23 +0100

    Commenting

--- TimeCheckerWPF5.0/Commands/CommandBase.cs
+++ TimeCheckerWPF5.0/Commands/CommandBase.cs
@@ -3,17 +3,50 @@ using System.Windows.Input;
 
 namespace TimeCheckerWPF5._0.Commands
 {
+
+    
+    /// Summary:
+    ///     A base class for all commands to implement ICommand's Execute, CanExecute and OnCanExcuteChanged
+    ///      which are the basic methods, generally a command should be able to "do"
+  
     public abstract class CommandBase : ICommand
     {
+
+
+        /// Summary:
+        ///     Occurs when changes occur that affect whether or not the command should execute.
         public event EventHandler CanExecuteChanged;
 
+
+        /// Summary:
+        ///     Defines the method that determines whether the command can execute in its current
+        ///     state.
+        ///
+        /// Parameters:
+        ///   parameter:
+        ///     Data used by the command. If the command does not require data to be passed,
+        ///
+        ///
+        /// Returns:
+        ///     true if this command can be executed; otherwise, false.
         public virtual bool CanExecute(object parameter)
         {
             return true;
         }
 
+        /// Summary:
+        ///     Defines the method to be called when the command is invoked.
+        ///
+        /// Parameters:
+        ///   parameter:
+        ///     Data used by the command. If the command does not require data to be passed,
+        ///     this object can be set to null.
         public abstract void Execute(object parameter);
 
+
+        /// Summary:
+        ///     Invokes the CanExecuteChanged EvenHandler with the command and its arguments to reconsider the CanExcute
+        ///     method
         protected void OnCanExecutedChanged()
         {
             CanExecuteChanged?.Invoke(this, new EventArgs());
--- TimeCheckerWPF5.0/Commands/DelegateCommand.cs
+++ TimeCheckerWPF5.0/Commands/DelegateCommand.cs
@@ -3,25 +3,58 @@ using System.Windows.Input;
 
 namespace TimeCheckerWPF5._0.Commands
 {
+
+    
+    /// Summary:
+    ///     A general Delegation Command that offers the CommandBase implementation to any command
+    ///     This way each command can implement its own Execute and CanExecute logic
+        
     public class DelegateCommand : CommandBase
     {
+
         private readonly Action<object> execute;
         private readonly Predicate<object> canExecute;
 
+        /// Summary:
+        /// Initializes a new instance of a Delegate Command asking for the canExecute logic and the
+        /// execute logic of the command plans to execute
+        ///         
+        /// Parameters:
+        ///   canExecute:
+        ///     The criteria the canExecute must match to execute
+        ///    execute:
+        ///     The implementation of the execute logic
         public DelegateCommand(Predicate<object> canExecute, Action<object> execute)
         {
             this.canExecute = canExecute;
             this.execute = execute;
         }
 
+
+
+        /// Summary:
+        /// Initializes a new instance of a Delegate Command asking just for the execute logic
+        /// of the command plans to execute. An instance without checking, if its executable
+        ///         
+        /// Parameters:
+        ///    execute:
+        ///     The implementation of the execute logic
         public DelegateCommand(Action<object> execute)
         {
             this.execute = execute;
         }
 
+
+        /// Summary:
+        ///     overrides the CanExecute method. Check if the command is executable
+        ///     based on its predicated result given by the calling command.
+        ///         
+        /// Parameters:
+        ///    execute:
+        ///     The implementation of the execute logic
         public override bool CanExecute(object parameter)
         { 
-            if (canExecute == null)
+            if (canExecute != null)
             {
                 canExecute?.Invoke(parameter);
 
