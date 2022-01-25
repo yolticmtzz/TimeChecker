c8a78acdf0e32083940d3efea0d75930be599a47
CptDestiny <stef.guler@gmail.com>
2022-01-25 13:45:45 +0100

    Commenting Navigation

--- TimeCheckerWPF5.0/MainWindow.xaml.cs
+++ TimeCheckerWPF5.0/MainWindow.xaml.cs
@@ -1,25 +1,46 @@
 ﻿using System.Windows;
-using TimeCheckerWPF5._0.Stores;
 using TimeCheckerWPF5._0.ViewModels;
 
 namespace TimeCheckerWPF5._0
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Injects the MainView and sets it as DataContext
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly NavigationStore _navigationStore;
-
-        public MainWindow(MainViewModel viewModel, NavigationStore navigationStore)
+        /// <summary>
+        /// Initializes a new instance of the MainView 
+        /// and sets the injected MainViewModel from StartUp as DataContext
+        /// Also registers an ExitEventHandler as ExitApp method
+        /// to the exit current application logic.
+        /// 
+        /// <paramref name="viewModel">
+        ///     injection of a MainViewModel from the StartUp method in
+        ///     the app xaml (DI).
+        /// />
+        /// </summary>
+        public MainWindow(MainViewModel viewModel)
         {
-            _navigationStore = navigationStore;
+            
             DataContext = viewModel;
             InitializeComponent();
 
             Application.Current.Exit += new ExitEventHandler(ExitApp);
         }
 
+        /// <summary>
+        /// Implements a MessageBox Pop uf, when the Application is terminated  
+        ///
+        /// <paramref name="sender">
+        ///     the object with what the application is closed (f.e. close-button)
+        /// </paramref>
+        /// 
+        /// <paramref name="e">
+        ///     the ExitEventArgs, irrelevant in this implementation
+        /// </paramref>
+        /// 
+        /// </summary>
         private void ExitApp(object sender, ExitEventArgs e)
         {
             MessageBox.Show("TimeChecker was terminated");
--- TimeCheckerWPF5.0/Services/NavigationService.cs
+++ TimeCheckerWPF5.0/Services/NavigationService.cs
@@ -4,6 +4,13 @@ using TimeCheckerWPF5._0.ViewModels;
 
 namespace TimeCheckerWPF5._0.Services
 {
+
+    /// <summary>
+    ///     A service, that is provided to the application to handle navigation mechanisms
+    ///     It uses the navigationStore and the serviceProvider from DI to set the
+    ///     targeted ViewModel as the current View Model in the navigationStore, where
+    ///     the datacontext's current viewmodel is defined.
+    /// </summary>
     public class NavigationService : INavigationService
         
     {
@@ -11,12 +18,39 @@ namespace TimeCheckerWPF5._0.Services
         private readonly IServiceProvider _serviceProvider;
 
 
+        /// <summary>
+        ///     Initializes a new instance of a NavigationService and
+        ///     injects the navigationStore and the serviceProvider
+        ///     
+        /// <paramref name="navigationStore">
+        ///     injects the navigationStore from DI
+        /// </paramref>
+        ///     injects the serviceProvider from DI
+        /// <paramref name="serviceProvider">
+        /// </paramref>
+        /// 
+        /// </summary> 
         public NavigationService(NavigationStore navigationStore, IServiceProvider serviceProvider)
         {
             _navigationStore = navigationStore;
             _serviceProvider = serviceProvider;
         }
 
+        /// <summary>
+        ///     Navigates to the targeted View based on setting the
+        ///     targetes viewModel to the currentViewModel in the navigationStore
+        ///     (thats how the datacontext in the MainView is bound)
+        /// 
+        /// Exceptions:
+        ///     To avoid navigating to something else than a UI ViewModel, 
+        ///     the navigationtarget's model is checked to be a ViewModel of
+        ///     ViewModelBase. Because only UI ViewModels inherit from ViewModelBase.
+        /// 
+        /// <paramref name="type">
+        ///     ir requires the type of ViewModel where it should navigate to.
+        /// </paramref>
+        /// 
+        /// </summary> 
         public void NavigateToType(Type type)
         {
             try
